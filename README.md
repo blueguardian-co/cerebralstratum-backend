eac08f1f958a72ef6f51823c977b443623d89762
Alexander Hussey <alex@HUSSDOGG.com>
2023-01-02 10:07:49 +1100

    package backend using setuptools

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # CEREBRAL STRATUM - Backend
 
+## Setup dev environment
+
+```bash
+# Create and activate a virtual environment to dev in
+python3 -m venv venv
+source venv/bin/activate
+# Install the application in editable mode
+python3 -m pip install -e .
+```
+
 ## How do?
 
 ```bash
-flask --app src.__main__ run -h localhost -p 6443
+# Start the API backend using the Werkzeug development server
+cs-api
 ```
--- src/__main__.py
+++ src/__main__.py
@@ -3,12 +3,28 @@
 """
 CEREBRALSTRATUM Flask Based Backend
 """
+from os import environ
+
+from authlib.integrations.flask_client import OAuth
+from flask import Flask
+from flask_socketio import SocketIO
 
 from src.versions.v1 import v1
 from src.versions.v2 import v2
 
-from flask import Flask
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = environ.get('OAUTH_CLIENT_KEY', 'SECRET1!')
 app.register_blueprint(v1)
 app.register_blueprint(v2)
+socketio = SocketIO(app)
+
+oauth = OAuth(app)
+
+def main():
+    """
+    Main application loop
+    """
+    socketio.run(app, host="127.0.0.1", port=6443)
+
+if __name__ == '__main__':
+    main()
