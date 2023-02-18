e3ae2446c74ac160bbb98b66cd8c5601534ff261
Alex Hussey <alex@blueguardian.co>
2023-02-19 09:19:11 +1100

    add OpenAPI spec + swagger

---
+++ cs-api/src/main/java/co/blueguardian/api/v1/Ping.java
@@ -0,0 +1,13 @@
+package co.blueguardian.api.v1;
+
+public class Ping {
+
+    public String response;
+
+    public Ping() {
+    }
+
+    public Ping(String response) {
+        this.response = response;
+    }
+}
---
+++ cs-api/src/main/java/co/blueguardian/api/v1/PingResource.java
@@ -0,0 +1,18 @@
+package co.blueguardian.api.v1;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/api/v1/ping")
+public class PingResource {
+
+    private Ping ping = new Ping();
+    public PingResource() {
+        ping = new Ping("pong");
+    }
+
+    @GET
+    public Ping returnPingStatus() {
+        return ping;
+    }
+}
