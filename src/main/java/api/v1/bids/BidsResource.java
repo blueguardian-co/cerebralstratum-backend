8eab6998957181e05756bc42dce5709409618be8
Alex Hussey <ahussey@redhat.com>
2024-07-09 06:27:06 +0000

    Updates to compile successfully

--- src/main/java/api/v1/bids/BidsResource.java
+++ src/main/java/api/v1/bids/BidsResource.java
@@ -1,17 +1,25 @@
-package org.acme.security.keycloak.authorization;
+package api.v1.bids;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
-import org.jboss.resteasy.reactive.NoCache;
-
+import io.quarkus.security.Authenticated;
 import io.quarkus.security.identity.SecurityIdentity;
 
 @Path("/api/v1/bids")
 @Authenticated
-@Inject
 public class BidsResource {
+    @Inject
     SecurityIdentity identity;
     private final String username = identity.getPrincipal().getName();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "All bids";
+    }
+
 }
