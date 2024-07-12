b0f23b50039bdd56aab614aa6b115525d65ea00a
Alex Hussey <ahussey@redhat.com>
2024-07-12 07:54:39 +0000

    Build out endpoints

---
+++ src/main/java/api/v1/bids/Bids.java
@@ -0,0 +1,97 @@
+package api.v1.bids;
+
+import java.time.LocalDateTime;
+
+import api.v1.auctions.Auctions;
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.QueryHint;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "bids")
+@NamedQuery(name = "Bids.findAll", query = "SELECT a FROM Bids a ORDER BY a.auction", hints = @QueryHint(name = "org.hibernate.cacheable", value = "true"))
+@Cacheable
+public class Bids {
+
+    @Id
+    @SequenceGenerator(name = "bidsSequence", sequenceName = "bids_id_seq", allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "bidsSequence")
+    private Integer id;
+
+    @ManyToOne
+    @PrimaryKeyJoinColumn
+    private Auctions auction;
+
+    @Column(length = 240)
+    private String username;
+
+    @Column(columnDefinition="timestamp")
+    private LocalDateTime bid_time;
+
+    @Column
+    private int bid_amount;
+
+    public Bids() {
+    }
+
+    public Bids(
+        Auctions auction,
+        String username,
+        LocalDateTime bid_time,
+        int bid_amount
+    ) {
+        this.auction = auction;
+        this.username = username;
+        this.bid_time = bid_time;
+        this.bid_amount = bid_amount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Auctions getAuction() {
+        return auction;
+    }
+
+    public void setAuction(Auctions auction) {
+        this.auction = auction;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDateTime getBid_time() {
+        return bid_time;
+    }
+
+    public void setBid_time(LocalDateTime bid_time) {
+        this.bid_time = bid_time;
+    }
+
+
+    public int getBid_amount() {
+        return bid_amount;
+    }
+
+    public void setBid_amount(int bid_amount) {
+        this.bid_amount = bid_amount;
+    }
+}
---
+++ src/main/java/api/v1/user/User.java
@@ -0,0 +1,64 @@
+package api.v1.user;
+
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.QueryHint;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_info")
+@NamedQuery(name = "User.findAll", query = "SELECT a FROM User a ORDER BY a.username", hints = @QueryHint(name = "org.hibernate.cacheable", value = "true"))
+@Cacheable
+public class User {
+
+    @Id
+    @SequenceGenerator(name = "userSequence", sequenceName = "user_id_seq", allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "userSequence")
+    private Integer id;
+
+    @Column(length = 240, unique = true)
+    private String username;
+
+    @Column(length = 50)
+    private int table_number;
+
+    public User() {
+    }
+
+    public User(
+        String username,
+        int table_number
+    ) {
+        this.username = username;
+        this.table_number = table_number;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getTable_number() {
+        return table_number;
+    }
+
+    public void setTable_number(int table_number) {
+        this.table_number = table_number;
+    }
+}
