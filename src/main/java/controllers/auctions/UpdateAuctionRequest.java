377c4c46dacb3975646325b8da66fed4cd0ec4b2
Alex Hussey <ahussey@redhat.com>
2024-07-26 02:47:45 +0000

    fix: Add default constuctor for each Resource class

--- src/main/java/controllers/auctions/UpdateAuctionRequest.java
+++ src/main/java/controllers/auctions/UpdateAuctionRequest.java
@@ -3,21 +3,6 @@ package controllers.auctions;
 import java.time.LocalDateTime;
 
 public class UpdateAuctionRequest {
-    public UpdateAuctionRequest(
-        int id,
-        String item_name,
-        String description,
-        LocalDateTime auction_start,
-        LocalDateTime auction_end,
-        String image_path
-    ) {
-        this.id = id;
-        this.item_name = item_name;
-        this.description = description;
-        this.auction_start = auction_start;
-        this.auction_end = auction_end;
-        this.image_path = image_path;
-    }
 
     public int id;
     public String item_name;
@@ -25,4 +10,22 @@ public class UpdateAuctionRequest {
     public LocalDateTime auction_start;
     public LocalDateTime auction_end;
     public String image_path;
+
+    public UpdateAuctionRequest() {
+    }
+
+    public UpdateAuctionRequest(
+            int id,
+            String item_name,
+            String description,
+            LocalDateTime auction_start,
+            LocalDateTime auction_end,
+            String image_path) {
+        this.id = id;
+        this.item_name = item_name;
+        this.description = description;
+        this.auction_start = auction_start;
+        this.auction_end = auction_end;
+        this.image_path = image_path;
+    }
 }
--- src/main/java/controllers/bids/CreateBidRequest.java
+++ src/main/java/controllers/bids/CreateBidRequest.java
@@ -8,14 +8,16 @@ public class CreateBidRequest {
     public LocalDateTime bid_time;
     public int bid_amount;
 
-    public CreateBidRequest (
-        int user_id,
-        LocalDateTime bid_time,
-        int bid_amount
-    ) {
+    public CreateBidRequest() {
+    }
+
+    public CreateBidRequest(
+            int user_id,
+            LocalDateTime bid_time,
+            int bid_amount) {
         this.user_id = user_id;
         this.bid_time = bid_time;
         this.bid_amount = bid_amount;
     }
-    
+
 }
